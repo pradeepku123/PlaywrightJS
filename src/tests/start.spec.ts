86981d5bb643b59737011accaf5200ba5ec8538b
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2021-10-09 21:54:58 +0530

    Annotation- Skip,beforeEach,BeforeAll describe,test

--- src/tests/start.spec.ts
+++ src/tests/start.spec.ts
@@ -1,17 +1,24 @@
 import { test, expect } from "@playwright/test";
 import { testUser } from "../resource/user.testdata";
 
-test("login to application", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
+});
 
-  await page.goto("http://localhost:3000/signin");
+test.describe("Test Description", () => {
+  test("login to application", async ({ page }) => {
+    await page.click('input[name="username"]');
 
-  await page.click('input[name="username"]');
+    await page.fill('input[name="username"]', testUser.userName);
 
-  await page.fill('input[name="username"]', testUser.userName);
+    await page.click('input[name="password"]');
 
-  await page.click('input[name="password"]');
-
-  await page.fill('input[name="password"]', testUser.password);
-  
+    await page.fill('input[name="password"]', testUser.password);
+  });
+  test("TC-002  @TC", async ({ page }) => {
+    const status = () => {
+      console.log("Status");
+    };
+    status();
+  });
 });
